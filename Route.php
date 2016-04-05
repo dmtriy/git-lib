3142359312e4469ba3a913b5c876f6f5a2f158fc
dmtriy <dmtriy@inbox.ru>
2016-04-05 22:23:39 +0300

    Класс обработки ссылок

--- Route.php
+++ Route.php
@@ -5,18 +5,20 @@
  * User: Дмитрий
  * Date: 28.03.2016
  * Time: 10:02
- *      private $controler; - выходной параметр контролер
-        private $module; - выходной параметр модуль
-        private $param = array(); - выходной параметр в виде массива ключ = имя параметра, значение = значение ключа
-
-        private static $CONTROLER_DEFAULT; - контролер по умолчанию
-        private static $MODULE_DEFAULT; - модуль по умолчанию
-
-        initRoute($cont='index', $mod='') метод установки значений $CONTROLER_DEFAULT; и $MODULE_DEFAULT;
-
-        parseUrl() метод устанавливаюший значения $controler ,$module и $param
-
-        Возврат значений методами getControler(),getModule(),getParam().
+ *
+ * private $controler; - выходной параметр контролер
+ * private $module; - выходной параметр модуль
+ * private $param = array(); - выходной параметр в виде массива ключ = имя параметра, значение = значение ключа
+ *
+ * private static $CONTROLER_DEFAULT; - контролер по умолчанию
+ * private static $MODULE_DEFAULT; - модуль по умолчанию
+ *
+ * initRoute($cont='index', $mod='') метод установки значений $CONTROLER_DEFAULT; и $MODULE_DEFAULT;
+ *
+ * parseUrl() метод устанавливаюший значения $controler ,$module и $param
+ *
+ * Возврат значений методами getControler(),getModule(),getParam().
+ *
  */
 class Route
 {
