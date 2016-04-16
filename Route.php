24b643986607fbeef5b6edd10d0c40f41efded15
dmtriy <dmtriy@inbox.ru>
2016-04-16 07:01:11 +0300

    Класс обработки ссылок

--- Route.php
+++ Route.php
@@ -12,6 +12,7 @@
  *
  * private static $CONTROLER_DEFAULT; - контролер по умолчанию
  * private static $MODULE_DEFAULT; - модуль по умолчанию
+ * private static $WITHOUT_MODEL; - система без модулей только контролеры
  *
  * initRoute($cont='index', $mod='') метод установки значений $CONTROLER_DEFAULT; и $MODULE_DEFAULT;
  *
@@ -28,6 +29,7 @@ class Route
 
     private static $CONTROLER_DEFAULT;
     private static $MODULE_DEFAULT;
+    private static $WITHOUT_MODEL;
 
 
     /**
@@ -55,14 +57,16 @@ class Route
     }
 
     /**
-     * @param $cont
-     * @param $mod
-     * Установка значений контролера и модели
+     * @param string $cont
+     * @param string $mod
+     * Установка значений контролера и модели по умолчанию
+     * @param $statMod
      */
-    public function initRoute($cont='index', $mod='')
+    public function initRoute($cont='index', $mod = '',$statMod = false )
     {
         self::$CONTROLER_DEFAULT = $cont;
         self::$MODULE_DEFAULT = $mod;
+        self::$WITHOUT_MODEL = $statMod;
     }
 
     /**
@@ -77,7 +81,8 @@ class Route
             $URL_Path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
             $URL_Parts = explode('/', trim($URL_Path, '/'));
             $this->controler = array_shift($URL_Parts);
-            $this->module = array_shift($URL_Parts);
+            if (self::$WITHOUT_MODEL)
+                $this->module = array_shift($URL_Parts);
             if (!empty(array_shift($URL_Parts))) {
                 for ($i = 0; $i < count($URL_Parts); $i++) {
                     $this->param[$URL_Parts[$i]] = $URL_Parts[++$i];
